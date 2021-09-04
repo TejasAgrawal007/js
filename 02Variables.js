b0b9520f679f25b82cee19c6dee5d52d15b9b370
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-04 22:45:53 +0530

    Add files via upload

---
+++ 02Variables.js
@@ -0,0 +1,46 @@
+console.log("I am in");
+
+/*
+ JavaScript Variabels
+
+1) Let
+2) Const
+3) Var
+
+
+ES6 -->  (2015)
+
+a = 5;
+console.log(a);
+
+
+
+
+// let a = 10;
+// console.log(a);
+
+*/
+
+
+//  Types of JS Variables
+
+//  1) var
+// var a = 5;
+// {
+//     a = 10;
+//     console.log(a);
+// }
+
+//  2) Let
+// {
+//     let b = 5;
+//     console.log(b);
+// }
+
+//  3) Const
+{
+    const a = 5;
+    // a = 10; // Wrong
+    console.log(a);
+}
+
