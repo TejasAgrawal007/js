2476f7d563967532111189c04a599baef8e8cab9
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-04 22:39:51 +0530

    first commit

---
+++ 01console.Object.js
@@ -0,0 +1,17 @@
+// 1. How to log something in Conole
+console.log('Hello I am Learning JS with Agrawal Code Wale!');
+
+// 2. error
+console.error("Hello I am a Error");
+
+// 3. warning
+console.warn("Hello i am a Warning");
+
+// 4. table
+console.table({name: 'Tejas', marks:34})
+
+// 5. Clear
+console.clear();
+
+// 6. timeEnd
+console.timeEnd();
