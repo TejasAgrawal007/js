c06b063d50d63a9b0555612263768e1f7ea2a464
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-06 17:18:39 +0530

    Add files via upload

---
+++ 03Operators.js
@@ -0,0 +1,23 @@
+console.log("Hello Guys Welcome to the Operators in JS");
+
+
+// 1. Arithmatic Operators
+// console.log("The Value of + Operator is",5+3);
+// console.log("The Value of - Operator is",5-3);
+// console.log("The Value of / Operator is",5/3);
+// console.log("The Value of % Operator is",5%3);
+// console.log("The Value of ** Operator is",5**3);
+
+
+// 2. JavaScipt Comarision operators
+
+
+// 3. Logical Operators
+/*
+
+1)  Logical and &&
+2) Logical or ||
+3) Logical Not !
+
+*/
+
