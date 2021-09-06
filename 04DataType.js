f2b9240af0d905ff14d10897c6aab78ee0d04821
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-06 17:18:52 +0530

    Add files via upload

---
+++ 04DataType.js
@@ -0,0 +1,14 @@
+console.log("Hello Guys Welcome back To the JS");
+
+// 1. Premitive Data Type
+// 2. Not-premitive Data Type / ReferenceData Type
+
+
+// Stack --> LIFO 
+// Queue--> FIFO   
+
+let str = "Hello I am a Programmer!";
+
+let abc = ['apple', 'banana', 'orange'];
+
+console.log(typeof(abc));
