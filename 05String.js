aebed8660108b3734c32f3beb471c34bef44366e
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-07 17:43:18 +0530

    Add files via upload

---
+++ 05String.js
@@ -0,0 +1,122 @@
+console.log("Welcome Guys To the JS Strings");
+
+
+// let bhushan = 'Best Coder';
+// console.log(bhushan);
+
+let str = 'Hello Guys Welcome Backto the JS master Series';
+
+
+
+// 1. Length
+// console.log(str.length);
+
+
+
+// ---***Extracting String parts***---
+
+// 2. slice
+// console.log(str.slice(0,-15))
+
+
+// 3. subString
+// console.log(str.substring(0,4))
+
+
+
+// 4. slice
+
+// let samyak = "Best progrmmer";
+// let result = samyak.slice(0,5);
+// console.log(result);
+
+
+// 5. Replace
+
+// let dambo = "Tejas is the Best Coder";
+// let result = dambo.replace("Tejas", "Arti");
+// console.log(result);
+
+
+// 6. toUpperCase()
+
+// let ayushi = "Ayushi is the Best Programmer in the entire universe";
+// console.log(ayushi.toUpperCase());
+
+
+
+// 7. toLowerCase()
+
+// let ayushi = "Ayushi is the Best Programmer in the entire universe";
+// console.log(ayushi.toLowerCase());
+
+
+// ---***Extracting String Characters***---
+
+
+// 8. CharAt
+
+// let abc = "Hello There";
+// let result = abc.charAt();
+// console.log(result);
+
+
+// 9. CharCodeAt()
+
+// let abc = "Hello There I am a Javascript coder";
+// let result = abc.charCodeAt(0);
+// console.log(result);
+
+
+
+// ---***JavaScript String  Search ***---
+
+
+
+// 10. indexOf()
+
+// let greeting = "Hello guys Have a Grate Day!";
+// console.log(greeting.indexOf('a'));
+
+
+// 11. lastIndexOf()
+
+// let classes = "MindSpace is the Best Class Ever";
+// console.log(classes.lastIndexOf('r'));
+
+
+// 12. Search
+
+// let google = "Best Search Engine";
+// console.log(google.search('Samyak'));
+
+
+// 13. include()
+
+// let ayushi = "Hello There i am Ayushi";
+// console.log(ayushi.includes('bhushan'));
+
+
+
+// 14. endsWith();
+
+// let arti = "Hello there i am Arti";
+// console.log(arti.endsWith('i'));
+
+
+// 15. Template Lierals
+
+
+// let name = "Tejas";
+
+// console.log(`${name} is the Best Coder`);
+
+
+// let name = "Tejas";
+// let data = '04-06-2022';
+
+// let greeting = `Hello mr ${name} sir/mam Happy to tell You
+//                 that you are selected to the google
+//                 as a Software enginners on date ${data}`;
+
+// console.log(greeting);
