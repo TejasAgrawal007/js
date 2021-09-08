a3238c45c122a94b528313685bbf5920cdb93ee9
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-08 16:15:01 +0530

    Add files via upload

---
+++ 06Array.js
@@ -0,0 +1,53 @@
+console.log("Hello Guys Welcome To The Arrays");
+
+
+// Objects
+
+// let a = {
+//     name: 'Tejas',
+//     age: 20,
+//     status: 'Programmer'
+// }
+
+// console.log(a);
+
+
+
+// 1. Creating an Array
+// let programmingLanguages = ['C', 'C++', 'Java', 'Paython'];
+// console.log(programmingLanguages);
+
+// 2. length
+// console.log(programmingLanguages.length);
+
+
+// 3. Index
+// console.log(programmingLanguages[1]);
+
+
+// 4. Typeof-Object
+// console.log(typeof(programmingLanguages));
+
+
+
+// 5. Assesing an Array
+const cars = ['BMW', 'Audi', 'Lambo', 'Jagwar', 'Marcidei'];
+// console.log(cars[3]);
+
+
+// 6.  Push
+let fruits = ['Mangoes', 'Orange'];
+fruits.push("Apple");
+console.log(fruits);
+
+// 7. unshift
+fruits.unshift("kiwi");
+console.log(fruits);
+
+// 8. pop()
+fruits.pop();
+console.log(fruits);
+
+// 9. shift()
+fruits.shift();
+console.log(fruits);
