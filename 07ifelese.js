b08b6ee54647baafe5b8d8efa3d69b77f3a668b6
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-09 17:48:27 +0530

    Add files via upload

---
+++ 07ifelese.js
@@ -0,0 +1,122 @@
+console.log("Welcome to the Condition Operatprs in JS");
+
+
+
+// 1. Syntex
+// if(condition){
+
+// }
+
+
+// let age = 20;
+
+// if(age == 20){
+//     console.log('Age is 20');
+// }
+
+
+
+
+// 2. Syntex
+// if(condition){
+
+// } else{
+
+// }
+
+// let age = 30;
+
+// if (age == 20) {
+//     console.log('Age is 20');
+// } else {
+//     console.log("You are not in your 20");
+// }
+
+
+//  3. Syntxt:
+
+// if(condition){
+
+// } else if{
+
+// } else{
+
+// }
+
+// const age = 30;
+
+// if (age == 19) {
+//     console.log("You are 19");
+// } else if (age == 20) {
+//     console.log("You are 20");
+// } else {
+//     console.log("you are not even 19 || 20");
+// }
+
+// Voting if else   
+
+
+// const age = 20;
+
+// if (age >= 18) {
+//     console.log("You are eligible to Vote");
+// }
+// else if (age < 18) {
+//     console.log("You are not eligible to Vote");
+// } else {
+//     console.log("You are not a human");
+// }
+
+
+
+// Check the Number is Positive or Negative
+
+// const number = prompt("Enter The number");
+
+// // Number is grater  than 0 or Not
+// if (number > 0) {
+//     console.log("Number is Positive");
+// } else if (number == 0) {
+//     console.log("You Enter the Number 0");
+// }
+// else {
+//     console.log("Number is Negative");
+// }
+
+
+// condition
+// 1. age 17 + 70 Your are ready drive 
+
+// const age = prompt("Enter Your Age");
+
+// if (age >= 70 || age <= 17) {
+//     console.log("you are not ready to Drive");
+// } else {
+//     console.log("you are ready to Drive");
+// }
+
+
+// let number = 50;
+// let userNumber = prompt("Enter Your Number");
+
+// if(number < userNumber){
+//     console.log("The Number is Grater than 50");
+// }
+// else if(number == userNumber){
+//     console.log("You Enter The Exact number 50");
+// }
+// else{
+//     console.log("The Number is Less than 50")
+// }
+
+
+// let time = prompt("Enter The Current Time");
+
+// if (time < 10) {
+//     console.log("Good Moring");
+// }
+// else if (time > 20) {
+//     console.log("Good Day");
+// } else {
+//     console.log("Good Evening");
+// }
