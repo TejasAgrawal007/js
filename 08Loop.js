029a8cd0813d5d143b00887934fe1b6ff3da7d7b
TejasAgrawal007 <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-10 17:48:06 +0530

    Add files via upload

---
+++ 08Loop.js
@@ -0,0 +1,95 @@
+console.log("Welcome to The Loop in JS guys");
+
+
+// 1. For_loop
+
+// for(let i = 0; i<50; i++){
+//     console.log("*");
+// }
+
+
+// 2. For ..in Loop
+
+// let Tejas = {
+//     name: 'Tejas',
+//     language: 'Javascript',
+//     age:20
+// }
+
+// for(let x in Tejas){
+//     console.log(x + " = " + Tejas[x]);
+// }
+
+
+// let languages = {
+//     name: 'Javascript',
+//     founder: 'Brendan',
+//     age: 70,
+// }
+
+
+// for (let i in languages) {
+//     console.log(i + " = " + languages[i]);
+// }
+
+
+
+//3 ForEach Loop in JS
+
+
+// let languages = ['c', 'c++', 'python', 'javascript', 'Dart'];
+
+// for(let i = 0;  i<languages.length; i++){
+//     console.log(languages[i]);
+// }
+
+// languages.forEach(bhushan => console.log(bhushan));
+
+
+// 4. While Loops
+
+// Syntex:
+// while (condition) {
+//     // statement...
+// }
+
+// let i = 1, n = 100;
+// while(i<=n){
+//     console.log(i);
+//     i++;
+// }
+
+
+// 5. Do-While Loop
+
+// Syntex:
+
+// do {
+
+// } while (condition);
+
+
+// let i = 1, n = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= n)
+
+
+// 6. Break
+
+// for(let i = 0; i<=100; i++){
+//     console.log(i);
+//     if(i === 50){
+//         break;
+//     }
+// }
+
+// 7. Continue
+
+// for(let i = 0; i<=100; i++){
+//     console.log(i);
+//     if(i === 50){
+//         continue;
+//     }
+// }
