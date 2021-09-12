596614490586822b12bbb8bece494f5af499276d
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-12 17:09:10 +0530

    Add files via upload

---
+++ 09Functions.js
@@ -0,0 +1,124 @@
+console.log("Hello guys Welcome To function is JS");
+
+// 1. Function
+
+// **__Syntex
+
+// function name(parameter){
+//     // statment...
+// }
+
+// function print_message(message){
+//     console.log(message);
+// }
+
+// print_message('Hello Tejas');
+// print_message('Hello Bhushan');
+// print_message('Hello Ayushi');
+// print_message('Hello Ariti');
+// print_message('Hello SAMYAK');
+
+
+// 2. Function Expression
+
+// __***Syntex
+
+// let variable_name = function(parameter){
+//     // Statement...
+// }
+
+
+// Example
+
+// let getSum = function(a,b){
+//     return a + b;
+// }
+
+// console.log(getSum(99,1));
+
+
+// 3. Return
+
+
+// Syntex:
+
+// function print_message(message) {
+//     console.log(message);
+// }
+
+// let result = 'hello Bhushan';
+// console.log('result ' + '= ' + result);
+
+
+
+
+// function getDistance(speed, time){
+//     let distance = speed * time;
+//     return distance;
+// }
+
+// let myDistance = getDistance(40,3);
+// console.log(myDistance);
+
+
+
+// Arrow Function 
+
+// syntex
+
+// let myFunction = (arg1, arg2) => {
+//     // code...
+// }
+
+
+// Addition
+
+// let addNum = function(p, q){
+//     return p + q;
+// }
+
+// console.log(addNum(5,15));
+
+
+// let Hello = function(){
+//     return "Hello MindSpace";
+// }
+
+// Hello();
+
+
+// function greet(){
+//     console.log("Good Eveing");
+// }
+
+// greet();
+
+
+// let greet = () => console.log("Good Evening");
+
+// greet();
+
+
+
+// greet = () => console.log("Hello Guys");
+
+// greet();
+
+
+
+// Quick Quize
+
+// let name;
+// let date;
+
+// function greeting(name, date) {
+//     console.log(`Hello mr.${name}, Happy to tell you that you are selected for an ABC compony on date ${date}. Have a Nice Day!`);
+// }
+
+// greeting('Suraj', '04-06-2022');
+
+
+
+// greeting = (name, date) => console.log(`Hello mr.${name}, Happy to tell you that you are selected for an ABC compony on date ${date}. Have a Nice Day!`);
+
+// greeting('Suraj', '04-06-2022');
