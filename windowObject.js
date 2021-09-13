537bd686989490faa094cc4cbbf7ae06c81e66f5
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-13 16:48:03 +0530

    Add files via upload

---
+++ windowObject.js
@@ -0,0 +1,39 @@
+console.log("Hello Guys Welcome to the Window Object");
+
+
+// let a = document;
+// console.log(a);
+
+// 2. innerWidth
+
+// let a = innerWidth;
+// console.log(a);
+
+
+// 3. innherHeight
+// let a = innerHeight;
+// console.log(a);
+
+
+// 4. location
+
+// let a = location;
+// console.log(a);
+
+
+
+// ___***Window Object Methods***___
+
+// 1. alert
+// alert("Hello Guys Don't Do that");
+
+
+// 2. prompt
+// let a = prompt("Enter Your Age");
+// console.log(a);
+
+
+// 3. confirm
+
+// let a = confirm("Are You sure you want to delate This File!");
+// console.log(a);
