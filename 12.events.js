170be858eaa6abb7cf8598ad335290a411b124d1
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-16 13:51:39 +0530

    Add files via upload

---
+++ 12.events.js
@@ -0,0 +1,16 @@
+console.log("Hello Guys Welcome to The Events in JS ");
+
+
+// const heading = document.querySelector('.heading')
+// const btn = document.querySelector('.btn')
+
+
+// btn.addEventListener('mouseover', function () {
+//     // console.log("You click The Button");
+
+//     heading.style.color = 'purple';
+//     heading.style.fontSize = '56px';
+//     heading.innerHTML = "Bhushan & samyak Replay Karo";
+// })
+
+
