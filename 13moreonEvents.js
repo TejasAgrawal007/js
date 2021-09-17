6ea18d047836e1b7bdc0b3f91c24e11fe51e3670
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-17 20:45:16 +0530

    Add files via upload

---
+++ 13moreonEvents.js
@@ -0,0 +1,60 @@
+console.log("Hello Guys Welcome To the More on Events in jS");
+
+// const heading = document.querySelector('.heading');
+// const btn = document.querySelector('.btn');
+
+
+// btn.addEventListener('click', function(){
+//     heading.innerHTML = "Hello Mindspace Good Moring!";
+//     heading.style.color = "purple" ;
+//     heading.style.fontSize = "80px" ;
+// })
+
+
+//  ***___Add a class___***
+// heading.classList.add('title');
+// heading.classList.remove('heading');
+
+
+
+//  ***___Advance manuplation___***
+
+
+// Creating a Element
+
+// const heading = document.createElement('h1');
+// heading.innerHTML = "Hello Tejas";
+// heading.classList.add('mindspace');
+// heading.style.color = "purple";
+
+
+// // Append
+
+// const parent = document.querySelector('.parent');
+// parent.appendChild(heading);
+
+
+
+// Ereating a Element
+
+// const para = document.createElement('p');
+// para.innerHTML = "Hello Guys Welcome to MindSpace";
+
+// para.classList.add('paragraph');
+// para.style.color = "red" ;
+// para.style.fontSize = "56px" ;
+
+
+// // AppendChild
+
+// const parent = document.querySelector('.parent');
+// parent.appendChild(para);
+
+
+
+
+
+
+
+
+
