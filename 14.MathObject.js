b81a5291b7575cb207a50dc7aca9a91dc79bec92
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-18 15:02:50 +0530

    Add files via upload

---
+++ 14.MathObject.js
@@ -0,0 +1,88 @@
+console.log("Hello Guys Welcome to Math Object in JS");
+
+// console.log(Math);
+
+
+
+
+// 1. Methods --> Methods
+
+// PI
+
+// console.log(Math.PI)
+
+// console.log(Math.E);
+
+// console.log(Math.sqrt(64));
+
+
+// 2. Math.round()
+
+
+// document.getElementById("mathObject").innerHTML = Math.round(4.9);
+// document.getElementById("mathObject").innerHTML = Math.round(4.4);
+
+
+// 3. min
+
+// document.getElementById("mathObject").innerHTML = Math.min(0, -150, 150, 1);
+
+// // 4. max
+
+// document.getElementById("mathObject").innerHTML = Math.max(0, -150, 150, 1);
+
+
+
+// 6. Random
+
+// console.log(Math.random());
+
+
+// 7. floor
+
+// -1
+// -2
+// -3
+// -4
+// -5
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+//  ****___JS Famous Bugs___***
+
+
+// true  --> 1
+// false --> 0
+
+
+// console.log(true + true);
+
+// NAN  
+// const abc = NaN;
+// console.log(abc);
+
+// console.log(typeof(abc));
+
+
+// let a = false;
+// console.log(a);
+// console.log(typeof (a));
+
+
+
+// console.log(Math.floor(11111.6));
+
+
+// 10. ceil
+
+// console.log(Math.ceil(7.9)); 
+
+
+
+
+// console.log(Math.random());
