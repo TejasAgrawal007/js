4c3dff3008bcaa5d5c49e43c2074785307a9247f
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2021-09-20 11:35:17 +0530

    Add files via upload

---
+++ 15Storage.js
@@ -0,0 +1,80 @@
+console.log("Hello Guys Welcome to Storage in JS");
+
+// Add a key-value pair inside the local Storage
+
+
+
+// #### Exapme of Key Value pair ####
+
+// let obj= {
+//     name: 'Tejas',
+//     age: 19
+// }
+
+// let storage = window.localStorage;
+// console.log(storage);
+
+
+
+
+
+
+// let storage = localStorage;
+// console.log(storage);
+
+
+// 1. setItem
+
+// localStorage.setItem('name1', 'Tejas');
+// localStorage.setItem('name2', 'Suraj');
+
+
+// 2. clear the particular key-vakue pair
+// localStorage.removeItem('name2');
+
+
+
+// 2. Clear the entire local Storage
+
+// 3. localStorage.clear();
+
+
+// let vegatables = ['bhendi', 'payaz', 'looki', 'tomato'];
+// vegatables.push('Potato');
+
+
+// localStorage.setItem('vegatables', JSON.stringify(vegatables));
+
+
+// 4. Retrieve an item form the localStorage
+
+// ## __getItem__ ##  
+
+// let name = localStorage.getItem('name1');
+// name = JSON.parse(localStorage.getItem('vegatables'));
+// console.log(name);
+
+
+// let name = localStorage.getItem('name2');
+// console.log(name);
+
+
+
+// 6. JSON.parse used to set an items into the arrays
+
+// let myVege = JSON.parse(localStorage.getItem('vegatables'));
+// console.log(myVege);
+
+
+
+
+// ****___Session Storage ___****
+
+
+// let session = sessionStorage;
+// console.log(session);
+
+// sessionStorage.setItem("Name",'MindSpace');
+// sessionStorage.setItem("Name2", 'Zayan');
+
+
